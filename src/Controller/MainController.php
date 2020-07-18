9dd08775e16c910b14b28f7569e84dad46fdaf80
Gilberto Cortez <me@gilbertocortez.com>
2020-07-17 21:39:31 -0700

    Add comments to controllers code

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,9 +9,13 @@ class MainController extends AbstractController
 {
     /**
      * @Route("/", name="Dashboard")
+     * 
+     * This is the home page / dashboard page
+     * 
      */
     public function index()
     {
+        // Render view from TWIG template
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
